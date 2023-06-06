0022b4d681f5600db9b2789e3fa280402aaf6b4f
RuslanKontrobaev <ruslan.kontrobaev@mail.ru>
2023-06-06 11:18:07 +0400

    Modified ValidatePassword method

--- PasswordChecker/Program.cs
+++ PasswordChecker/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace PasswordChecker
 {
@@ -10,6 +7,12 @@ namespace PasswordChecker
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Вводимый пароль обязан:\n" +
+                "1. Содержать символы верхнего и нижнего регистров.\n" +
+                "2. Состоять только из цифр, букв латинского алфавита, нижнего подчеркивания, тире, точки.\n" +
+                "3. В начале и в конце пароля должны находиться латинский символы.\n" +
+                "4. Длина пароля от 8 до 25 символов.\n");
+
             Console.Write("Введите пароль: ");
             string password = Console.ReadLine();
 
@@ -29,7 +32,10 @@ namespace PasswordChecker
 
         static bool ValidatePassword(string password)
         {
+            string pattetn = @"^[a-zA-Z][\w.-]{6,23}[a-zA-Z]$";
+            Regex regex = new Regex(pattetn);
 
+            return regex.IsMatch(password);
         }
     }
 }
